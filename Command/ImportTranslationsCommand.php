250c74e5ced48d39fd83c83458a0870152c59d26
Matthieu <rei_eva@hotmail.com>
2020-01-21 10:28:13 +0100

    pre-modif !8

--- Command/ImportTranslationsCommand.php
+++ Command/ImportTranslationsCommand.php
@@ -2,7 +2,7 @@
 
 namespace Lexik\Bundle\TranslationBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
