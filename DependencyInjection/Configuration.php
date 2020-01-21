7fa89941945caae3cd66f88ca62fcbeffd50cf6e
Matthieu <rei_eva@hotmail.com>
2020-01-21 09:51:50 +0100

    pre-modif !4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,7 +22,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('lexik_translation');
-        $rootNode = $treeBuilder->root();
+        $rootNode = $treeBuilder->getRootNode();
 
         $storages = array(
             StorageInterface::STORAGE_ORM,
